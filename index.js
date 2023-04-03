5e9fe621b5ddc47600e3b7d7994770abaa47e84c
daliakhalifa02 <daliakhalifa02@gmail.com>
2023-04-04 02:04:24 +0300

    Completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+// console.log(saturdayFun("go to the beach"))
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+// console.log(mondayWork("drink coffee"))
+
+
+function wrapAdjective(char = "*"){
+    return function result(emphatic = "a hard worker"){
+        return `You are ${char}${emphatic}${char}!`
+    }
+}
+// console.log(wrapAdjective('||')('a dedicated programmer'))
